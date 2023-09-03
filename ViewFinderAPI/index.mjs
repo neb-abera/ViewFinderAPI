6c9795757f4804df559f3e6f4b1e46e23c788faa
Neb Abera <29741322+neb-abera@users.noreply.github.com>
2023-09-02 23:15:04 -0500

    update lambda to use api and db V2

--- ViewFinderAPI/index.mjs
+++ ViewFinderAPI/index.mjs
@@ -1,147 +1,123 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import {
-  DynamoDBDocumentClient,
-  ScanCommand,
-  PutCommand,
-  GetCommand,
-  DeleteCommand,
-  UpdateCommand
+    DynamoDBDocumentClient,
+    ScanCommand,
+    PutCommand,
+    GetCommand,
+    DeleteCommand,
+    UpdateCommand,
+    QueryCommand
 } from "@aws-sdk/lib-dynamodb";
 
 const client = new DynamoDBClient({});
 
 const dynamo = DynamoDBDocumentClient.from(client);
 
-const tableName = "http-crud-movie-users-recommendations";
+const tableName = "http-crud-movie-users-recommendationsV2";
 
 export const handler = async (event, context) => {
-  let body;
-  let statusCode = 200;
-  const headers = {
-    "Content-Type": "application/json",
-  };
+    let body;
+    let statusCode = 200;
+    const headers = {
+        "Content-Type": "application/json",
+    };
 
-  try {
-    switch (event.routeKey) {
-      case "GET /users":
-        body = await dynamo.send(
-          new ScanCommand({
-            TableName: tableName
-          })
-        );
-        body = body.Items;
-        break;
-      case "GET /users/getUser/{userID}":
-        body = await dynamo.send(
-          new GetCommand({
-            TableName: tableName,
-            Key: {
-              userID: event.pathParameters.userID
-            },
-          })
-        );
-        body = body.Item;
-        break;
-      case "PUT /users/addUser/{userID}":
-        await dynamo.send(
-          new PutCommand({
-            TableName: tableName,
-            Item: {
-              "userID": event.pathParameters.userID,
-              "movies": {},
-            },
-          })
-        );
-        body = `Added user ${event.pathParameters.userID}`;
-        break;
-      case "DELETE /users/removeUser/{userID}":
-        await dynamo.send(
-          new DeleteCommand({
-            TableName: tableName,
-            Key: {
-              "userID": event.pathParameters.userID
-            },
-          })
-        );
-        body = `Deleted user ${event.pathParameters.userID}`;
-        break;
-      case "PUT /users/addMovie/{userID}":
-        let addMovieJSON = typeof event.body === 'string' ? JSON.parse(event.body) : event.body;
-        
-        var userID = event.pathParameters.userID;
-        var titleID = addMovieJSON.titleID;
-      
-        try {
-          await dynamo.send(
-            new UpdateCommand({
-              TableName: tableName,
-              Key: { "userID": userID },
-              UpdateExpression: "ADD movies.#titleID.recommendations :increment",
-              ConditionExpression: "attribute_exists(movies.#titleID)",
-              ExpressionAttributeNames: {
-                "#titleID": titleID,
-              },
-              ExpressionAttributeValues: {
-                ":increment": 1,
-              },
-              ReturnValues: "ALL_NEW",
-            })
-          );
-      
-          body = `Incremented recommendations for ${titleID} in ${userID}`;
-        } catch (error) {
-          if (error.name === "ConditionalCheckFailedException") {
-            await dynamo.send(
-              new UpdateCommand({
-                TableName: tableName,
-                Key: { "userID": userID },
-                UpdateExpression: "SET movies.#titleID = :details",
-                ExpressionAttributeNames: {
-                  "#titleID": titleID,
-                },
-                ExpressionAttributeValues: {
-                  ":details": addMovieJSON,
-                },
-                ReturnValues: "ALL_NEW",
-              })
-            );
-      
-            body = `Added movie to ${userID}`;
-          }
+    try {
+        switch (event.routeKey) {
+            case "GET /users":
+                body = await dynamo.send(
+                    new ScanCommand({
+                        TableName: tableName
+                    })
+                );
+                body = body.Items;
+                break;
+            case "GET /user/{userID}":
+                body = await dynamo.send(
+                    new QueryCommand({
+                        TableName: tableName,
+                        KeyConditionExpression: "#userID = :userID",
+                        ExpressionAttributeNames: {
+                            "#userID": "userID",
+                        },
+                        ExpressionAttributeValues: {
+                            ":userID": event.pathParameters.userID,
+                        },
+                    })
+                );
+                body = body.Items;
+                break;
+            case "PUT /user/{userID}/{titleID}":
+                let addMovieJSON = typeof event.body === 'string' ? JSON.parse(event.body) : event.body;
+                try {
+                    // Try to update the item
+                    await dynamo.send(
+                        new UpdateCommand({
+                            TableName: tableName,
+                            Key: {
+                                "userID": event.pathParameters.userID,
+                                "titleID": event.pathParameters.titleID,
+                            },
+                            UpdateExpression: "ADD #recommendations :increment",
+                            ExpressionAttributeNames: {
+                                "#recommendations": "recommendations",
+                            },
+                            ExpressionAttributeValues: {
+                                ":increment": 1,
+                            },
+                            ConditionExpression: "attribute_exists(userID)",
+                            ReturnValues: "ALL_NEW",
+                        })
+                    );
+                    body = `Incremented recommendations for ${event.pathParameters.titleID} in ${event.pathParameters.userID}`;
+                } catch (error) {
+                    if (error.name === "ConditionalCheckFailedException") {
+                        // Item does not exist, create it
+                        await dynamo.send(
+                            new PutCommand({
+                                TableName: tableName,
+                                Item: {
+                                    "userID": event.pathParameters.userID,
+                                    "titleID": event.pathParameters.titleID,
+                                    "rating": addMovieJSON.rating,
+                                    "recommendations": 0,
+                                    "watched": false,
+                                    "rejected": false,
+                                },
+                            })
+                        );
+                        body = `Added the movie ${event.pathParameters.titleID} to user ${event.pathParameters.userID}`;
+                    } else {
+                        throw error;
+                    }
+                }
+                break;
+            case "DELETE /user/{userID}/{titleID}":
+                await dynamo.send(
+                    new DeleteCommand({
+                        TableName: tableName,
+                        Key: {
+                            "userID": event.pathParameters.userID,
+                            "titleID": event.pathParameters.titleID,
+                        },
+                    })
+                );
+                body = `Deleted movie from user ${event.pathParameters.userID}`;
+                break;
+            default:
+                throw new Error(`Unsupported route: "${event.routeKey}"`);
         }
-        break;
-      case "DELETE /users/deleteMovie/{userID}":
-        let deleteMovieJSON = typeof event.body === 'string' ? JSON.parse(event.body) : event.body;
-        var userID = event.pathParameters.userID;
-        var titleID = deleteMovieJSON.titleID;
-
-        await dynamo.send(
-          new UpdateCommand({
-            TableName: tableName,
-            Key: { "userID": userID },
-            UpdateExpression: "REMOVE movies.#titleID",
-            ExpressionAttributeNames: {
-              "#titleID": titleID
-            },
-            ReturnValues: "ALL_NEW"
-          })
-        );
-        body = `Deleted movie #${titleID} with type ${typeof titleID === 'string'} from ${userID} with type ${typeof userID === 'string'}`;
-        break;
-      default:
-        throw new Error(`Unsupported route: "${event.routeKey}"`);
+    } catch (err) {
+        statusCode = 400;
+        body = err.message;
+    } finally {
+        body = JSON.stringify(body);
     }
-  } catch (err) {
-    statusCode = 400;
-    body = err.message;
-  } finally {
-    body = JSON.stringify(body);
-  }
-  
 
-  return {
-    statusCode,
-    body,
-    headers,
-  };
+
+    return {
+        statusCode,
+        body,
+        headers,
+    };
 };
